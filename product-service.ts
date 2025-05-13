0bdabf5e7c3130424c4662ee55bd510df181fbee
infyprj <naturalmovies267@gmail.com>
2025-05-13 15:19:43 +0530

    Update product-service.ts

--- product-service.ts
+++ product-service.ts
@@ -1,1 +1,11 @@
+export interface IProduct {
+  productId: number
+  name: string
+  description: string
+  price: number
+  categoryId: number
+  modelUrl: string
+  thumbnailUrl: string
+  quantity: number
+}
 
