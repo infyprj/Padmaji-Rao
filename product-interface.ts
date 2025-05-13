f402cfa15212671ac307429d7ab71c18e85046d2
infyprj <naturalmovies267@gmail.com>
2025-05-13 15:18:59 +0530

    Create product-interface.ts

---
+++ product-interface.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  productId: number
+  name: string
+  description: string
+  price: number
+  categoryId: number
+  modelUrl: string
+  thumbnailUrl: string
+  quantity: number
+}
